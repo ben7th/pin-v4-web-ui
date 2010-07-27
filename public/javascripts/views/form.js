f666f6d0d1a6d25b7e957c347f367467b91b871a
ben7th <ben7th@sina.com>
2010-07-27 16:23:36 +0800

    index

---
+++ public/javascripts/views/form.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  // 添加classname到对应的form field
+  $('html body input[type="text"], html body input[type="password"]').addClass("text");
+  $('input[type="submit"]').addClass("submit");
+  
+  // 给校验中出现error的字段添加样式
+  $("form .fieldWithErrors").closest("div.field").addClass("error")
+
+  // -- add active class to active elements
+  $("form select, form .text, form textarea")
+  .focus(function( ){
+    $(this).closest("div.field").addClass("active");
+    $(this).closest("fieldset").addClass("active");
+  })
+  .blur(function( ){
+    $(this).closest("div.field").removeClass("active");
+    $(this).closest("fieldset").removeClass("active");
+  });
+  
+  // -- make error notice the same width as error field
+  $("form .fieldWithErrors input, form .fieldWithErrors textarea").each(function(i, field){
+    width = $(field).width();
+    $(field).closest('div.field').find('.formError').width(width);
+  });
+});
+
+
