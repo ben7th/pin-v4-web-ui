1d1fc246f5d49a17b9d8cf2646ffb39075e78694
ben7th <ben7th@sina.com>
2010-08-19 13:41:03 +0800

    facebox

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -16,46 +16,6 @@ module PageHelper
       return ex
     end
   end
-
-  # 获取当前页面显示主题字符串，如果没有默认是 'sapphire'
-  def get_user_theme_str
-    if @mindpin_layout.theme.blank?
-      user = current_user
-
-      # 个人主页特殊处理
-      if [controller_name,action_name] == ["users","show"]
-        user = User.find(params[:id])
-      end
-
-      return "sapphire" if user.blank?
-
-      user.create_preference if user.preference.blank?
-      theme = user.preference.theme
-      theme = "sapphire" if theme.blank?
-      return theme
-    else
-      @mindpin_layout.theme
-    end
-  end
-
-  # 获取页面布局的container样式名
-  def container_classname
-    @get_container_classname ||= _layout_classname('container')
-  end
-  
-  def head_classname
-    @mindpin_layout.head_class || ''
-  end
-
-  # 获取页面布局的grid样式名
-  def grid_classname
-    @get_grid_classname ||= _layout_classname('grid')
-  end
-
-  def _layout_classname(prefix)
-    return "#{prefix}_#{@mindpin_layout.grid}" if @mindpin_layout.grid
-    return ''
-  end
   
   def render_tabs
     tabs_path = controller.class.name.downcase.sub('::','/').sub('controller','/tabs')
